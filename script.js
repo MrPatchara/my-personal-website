c65107795dde0d830253ac3c2f1419908e917159
Patchara Al-umaree <patcharaalumaree@gmail.com>
2024-06-06 00:20:52 +0700

    up

--- script.js
+++ script.js
@@ -29,18 +29,4 @@ scrollToTopBtn.addEventListener('click', function () {
 
 
 
-// Animated Statistics Counter
-$(window).scroll(function () {
-    var hT = $('#skills').offset().top,
-        hH = $('#skills').outerHeight(),
-        wH = $(window).height(),
-        wS = $(this).scrollTop();
-    if (wS > (hT + hH - wH)) {
-        $('.progress-bar').each(function () {
-            $(this).css('width', $(this).attr('aria-valuenow') + '%');
-        });
-    }
-});
-
-
 
